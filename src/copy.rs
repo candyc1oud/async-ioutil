1378750242472bebc11f1f5a414c39052f4bc67a
Candy Cloud <candyc1oud@outlook.com>
2022-09-23 09:50:48 +0000

    refactor: use trait `AsyncRead` and `AsyncWrite`

--- src/copy.rs
+++ src/copy.rs
@@ -1,12 +1,12 @@
-use futures::{AsyncReadExt, AsyncWriteExt};
+use futures::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use std::io::Result;
 use std::marker::Unpin;
 
 /// Copy data from reader to writer.
 pub async fn copy_buffer<R, W>(reader: &mut R, writer: &mut W, buffer: &mut [u8]) -> Result<usize>
 where
-    R: AsyncReadExt + Unpin,
-    W: AsyncWriteExt + Unpin,
+    R: AsyncRead + Unpin,
+    W: AsyncWrite + Unpin,
 {
     let mut n = 0;
     loop {
@@ -23,8 +23,8 @@ where
 /// Copy data from reader to writer.
 pub async fn copy<R, W>(reader: &mut R, writer: &mut W) -> Result<usize>
 where
-    R: AsyncReadExt + Unpin,
-    W: AsyncWriteExt + Unpin,
+    R: AsyncRead + Unpin,
+    W: AsyncWrite + Unpin,
 {
     const BUF_SIZE: usize = 32 * 1024;
     let mut buffer = [0u8; BUF_SIZE];
--- src/read.rs
+++ src/read.rs
@@ -1,9 +1,9 @@
-use futures::AsyncReadExt;
+use futures::{AsyncRead, AsyncReadExt};
 use std::io::Result;
 use std::marker::Unpin;
 
 /// Read all data into `Vec<u8>`.
-pub async fn read_all<R: AsyncReadExt + Unpin>(reader: &mut R) -> Result<Vec<u8>> {
+pub async fn read_all<R: AsyncRead + Unpin>(reader: &mut R) -> Result<Vec<u8>> {
     let mut bytes: Vec<u8> = Vec::new();
     reader.read_to_end(&mut bytes).await?;
     Ok(bytes)
